84dfb11a8a9670545c29cc50467ee185580e5d16
Aleksandar Zeljkovic <aleksandar.zeljkovic91@gmail.com>
2022-10-09 14:00:51 +0200

    add real api to modal

--- src/Details.jsx
+++ src/Details.jsx
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 import Carousel from "./Carousel.jsx";
 import ErrorBoundary from "./ErrorBoundary.jsx";
 import ThemeContext from "./ThemeContext.jsx";
+import Modal from "./Modal.jsx";
 
 // const Details = () => {
 //   const { id } = useParams();
@@ -12,7 +13,7 @@ import ThemeContext from "./ThemeContext.jsx";
 // export default Details;
 
 class Details extends Component {
-  state = { loading: true };
+  state = { loading: true, showModal: false };
 
   async componentDidMount() {
     const res = await fetch(
@@ -23,6 +24,8 @@ class Details extends Component {
     this.setState(Object.assign({ loading: false }, json.pets[0]));
   }
 
+  toggleModal = () => this.setState({ showModal: !this.state.showModal });
+
   render() {
     if (this.state.loading) {
       return <h2>loading...</h2>;
@@ -31,15 +34,28 @@ class Details extends Component {
     // ErrorBoundary test
     // throw new Error("crashed x.x");
 
-    const { animal, breed, city, state, description, name, images } = this.state;
+    const { animal, breed, city, state, description, name, images, showModal } = this.state;
     return (
       <div className="details">
         <Carousel images={images} />
         <div>
           <h1>{name}</h1>
           <h2>{animal} - {breed} - {city}, {state}</h2>
-          <button style={{ backgroundColor: this.props.theme }}>Adopt {name}</button>
+          <button onClick={this.toggleModal} style={{ backgroundColor: this.props.theme }}>Adopt {name}</button>
           <p>{description}</p>
+          {
+            showModal ? (
+              <Modal>
+                <div>
+                  <h1>Would you like to adopt {name}?</h1>
+                  <div className="buttons">
+                    <a href="https://bit.ly/pet-adopt">Yes</a>
+                    <button onClick={this.toggleModal}>No</button>
+                  </div>
+                </div>
+              </Modal>
+            ) : null
+          }
         </div>
       </div>
     );
