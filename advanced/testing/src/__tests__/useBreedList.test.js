6960eb7c6d6bd431820376605b17c67a09db6ad8
Aleksandar Zeljkovic <aleksandar.zeljkovic91@gmail.com>
2022-10-18 22:20:26 +0200

    Added mocking for hook test.

--- advanced/testing/src/__tests__/useBreedList.test.js
+++ advanced/testing/src/__tests__/useBreedList.test.js
@@ -23,9 +23,35 @@ test("gives an empty list with no animal", async () => {
   // without @testing-library/react-hooks
   // const [breedList, status] = getBreedList();
 
-  const {result} = renderHook(() => useBreedList());
+  const { result } = renderHook(() => useBreedList());
   const [breedList, status] = result.current;
 
   expect(breedList).toHaveLength(0);
   expect(status).toBe("unloaded");
-})
+});
+
+test("gives back breeds with an animal", async () => {
+  const breeds = [
+    "Havanese",
+    "Bichon Frise",
+    "Poodle",
+    "Maltese",
+    "Golden Retriever",
+    "Labrador",
+    "Husky",
+  ];
+
+  fetch.mockResponseOnce(
+    JSON.stringify({
+      animal: "dog",
+      breeds,
+    })
+  );
+
+  const { result, waitForNextUpdate } = renderHook(() => useBreedList("dog"));
+  await waitForNextUpdate();
+
+  const [breedList, status] = result.current;
+  expect(status).toBe("loaded");
+  expect(breedList).toEqual(breeds);
+});
